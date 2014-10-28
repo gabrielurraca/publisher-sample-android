3ab4fe58a4d235856507e9ad8cec6e0ca4f62f8c
jordyn <jchuhaloff@gmail.com>
2014-10-27 17:59:35 -0700

    modified to run in eclipse and ant * still runs in android studio and gradle as well * still need to add README.md

--- src/main/java/com/vungle/publisherSampleAndroid/MainActivity.java
+++ src/main/java/com/vungle/publisherSampleAndroid/MainActivity.java
@@ -1,6 +1,5 @@
 package com.vungle.publisherSampleAndroid;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.app.Activity;
 import com.vungle.publisher.VunglePub;
